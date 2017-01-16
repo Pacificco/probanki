6d9ac35aec266e2e99059c26df31d612825ed3cc
Pacificco <pacificco@mail.ru>
2017-01-16 23:01:11 +0300

    Добавил поп-ап для Авторизации

--- Blog/Models/Domain/Account/VM_UserLogin.cs
+++ Blog/Models/Domain/Account/VM_UserLogin.cs
@@ -9,7 +9,7 @@ namespace Bankiru.Models.Domain.Account
     public class VM_UserLogin
     {
         [Required(ErrorMessage = "Укажите Email!")]
-        [Display(Name = "Email")]
+        [Display(Name = "Эл. почта (Email)")]
         //[DataType(System.ComponentModel.DataAnnotations.DataType.EmailAddress, ErrorMessage ="Не корректный Email!")]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный Email!")]
         public string Username { get; set; }
